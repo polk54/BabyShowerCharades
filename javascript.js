e856af7393b53bc2264169c91cc39f412af35d65
Parul Agarwal <polk54@gmail.com>
2024-01-07 13:22:06 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,6 +3,6 @@ function newWord(){
   document.getElementById("wordDisplay").innerHTML = words[randomNumber];
 }
 
-var words = ["Diaper Change", "Peekaboo", "Pacifier", "Crawl", "Baby Powder", "Teething", 
+var words = ["Diaper Change", "Peekaboo", "Pacifier", "Crawl", "Teething", 
              "Rocking Chair", "Tummy Time", "Baby Steps", "Rubber Ducky", "Spit-up", 
-             "Potty Training", "Burp", "Giggle", "Diaper Blowout", "Potty Training"]
+             "Potty Training", "Burp", "Giggle", "Diaper Blowout"]
